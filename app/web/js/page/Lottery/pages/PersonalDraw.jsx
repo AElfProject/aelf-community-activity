d7158ef056bf57cdf937a2643597ce32966f23b9
xjzhang <>
2021-01-23 19:46:42 +0800

    time color black

--- app/web/js/page/Lottery/pages/PersonalDraw.jsx
+++ app/web/js/page/Lottery/pages/PersonalDraw.jsx
@@ -323,9 +323,8 @@ export default class PersonalDraw extends Component{
                 type="primary" onClick={() => this.onBuyClick()}>Switch</Button>
             </div>
             <div className="text-grey">
-              The Lucky Draw Function will be closed after <span className="grand-prize-error">
-              {switchCodeDate.end ? moment(switchCodeDate.end).format('YYYY-MM-DD HH:mm') : '-'}
-            </span>. Please switch it in time to avoid loss.
+              The Lucky Draw Function will be closed after&nbsp;
+              {switchCodeDate.end ? moment(switchCodeDate.end).format('YYYY-MM-DD HH:mm') : '-'}. Please switch it in time to avoid loss.
             </div>
           </div>
 
